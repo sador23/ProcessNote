7fe56856f108b1aa3f98a09d934e3d131e5c3065
sador23 <adorjns@gmail.com>
2017-06-13 13:35:53 +0200

    Base logic done

--- ProcessNote/Form1.Designer.cs
+++ ProcessNote/Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.processes = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.details = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.processes);
+            this.panel1.Location = new System.Drawing.Point(12, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(243, 322);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Running Processes:";
+            // 
+            // processes
+            // 
+            this.processes.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.processes.FormattingEnabled = true;
+            this.processes.Location = new System.Drawing.Point(7, 42);
+            this.processes.Name = "processes";
+            this.processes.Size = new System.Drawing.Size(167, 277);
+            this.processes.TabIndex = 0;
+            this.processes.Click += new System.EventHandler(this.processes_Click);
+            this.processes.SelectedIndexChanged += new System.EventHandler(this.processes_SelectedIndexChanged);
+            this.processes.DoubleClick += new System.EventHandler(this.processes_DoubleClick);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.details);
+            this.panel2.Location = new System.Drawing.Point(302, 69);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 263);
+            this.panel2.TabIndex = 1;
+            // 
+            // details
+            // 
+            this.details.FormattingEnabled = true;
+            this.details.Location = new System.Drawing.Point(16, 25);
+            this.details.Name = "details";
+            this.details.Size = new System.Drawing.Size(171, 186);
+            this.details.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(683, 475);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox processes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox details;
     }
 }
 
--- ProcessNote/Form1.cs
+++ ProcessNote/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,59 @@ namespace ProcessNote
         public Form1()
         {
             InitializeComponent();
+            processes.ScrollAlwaysVisible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Process> processlist = Process.GetProcesses().ToList();
+            foreach(Process processelement in processlist){
+                processes.Items.Add(processelement.Id + "  " + processelement.ProcessName);
+            }
+
+        }
+
+        private void processes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void processes_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void processes_DoubleClick(object sender, EventArgs e)
+        {
+
+            if (processes.SelectedItem != null)
+            {
+                String id = processes.SelectedItem.ToString().Split(' ')[0];
+                foreach (Process processelement in Process.GetProcesses().ToList())
+                {
+                    
+                    if(processelement.Id == int.Parse(id))
+                    {
+                        details.Items.Clear();
+                        try
+                        {
+
+                        details.Items.Add("Processor details:");
+                        details.Items.Add(string.Format("Total       {0}",
+                        processelement.TotalProcessorTime));
+                        details.Items.Add(string.Format("User        {0}",
+                        processelement.UserProcessorTime));
+                        details.Items.Add(string.Format("Privileged  {0}",
+                        processelement.PrivilegedProcessorTime));
+                        details.Items.Add("Memory usage:");
+                        details.Items.Add(string.Format("Current     {0:N0} B", processelement.WorkingSet64));
+                        details.Items.Add(string.Format("Peak        {0:N0} B", processelement.PeakWorkingSet64));
+                        details.Items.Add(string.Format("Active threads      {0:N0}", processelement.Threads.Count));
+                        }
+                        catch (Win32Exception) { MessageBox.Show("Unable to access the process! Try another one"); }
+                        }
+                }
+            }
         }
     }
 }
